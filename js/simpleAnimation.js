ae23a3455bb7bd1fd09f10a81fe8779424f4b1f7
CamiloWG <millos2423@gmail.com>
2022-05-18 15:58:40 -0500

    Added a cute animation

---
+++ js/simpleAnimation.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed("#names", {
+    strings: [" ","Camilo Madero", "Valentina Andrade", "Nicolas Ramirez", "Paula Paez", "Laura Gonzalez"],
+    typeSpeed: 50,
+    BackSpeed: 30,
+    loop: true
+});
+
