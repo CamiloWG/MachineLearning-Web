9c2f80ad5a1301f9479151939364909b5a32eaed
CamiloWG <millos2423@gmail.com>
2022-05-18 20:22:37 -0500

    IA Sound added

--- js/soundRecognizer.js
+++ js/soundRecognizer.js
@@ -3,6 +3,16 @@
 
 // the link to your model provided by Teachable Machine export panel
 
+const DoBar = document.getElementById("DO");
+const ReBar = document.getElementById("RE");
+const MiBar = document.getElementById("MI");
+const FaBar = document.getElementById("FA");
+const SolBar = document.getElementById("SOL");
+const LaBar = document.getElementById("LA");
+const SiBar = document.getElementById("SI");
+const NoiseBar = document.getElementById("Noise");
+
+
 const button = document.getElementById("btnStart");
 
 const URL = "https://camilowg.github.io/MachineLearning-Web/model/sound/";
@@ -30,10 +40,10 @@ button.addEventListener("click", () => {
 async function init() {
     const recognizer = await createModel();
     const classLabels = recognizer.wordLabels(); // get class labels
-    const labelContainer = document.getElementById("label-container");
-    for (let i = 0; i < classLabels.length; i++) {
-        labelContainer.appendChild(document.createElement("div"));
-    }
+    // const labelContainer = document.getElementById("label-container");
+    // for (let i = 0; i < classLabels.length; i++) {
+    //     labelContainer.appendChild(document.createElement("div"));
+    // }
 
     // listen() takes two arguments:
     // 1. A callback function that is invoked anytime a word is recognized.
@@ -42,8 +52,41 @@ async function init() {
         const scores = result.scores; // probability of prediction for each class
         // render the probability scores per class
         for (let i = 0; i < classLabels.length; i++) {
-            const classPrediction = classLabels[i] + ": " + result.scores[i].toFixed(2);
-            labelContainer.childNodes[i].innerHTML = classPrediction;
+            let predictionAmount = result.scores[i].toFixed(2) * 100;
+            switch (classLabels[i]) {
+                case "DO":
+                    DoBar.style.width = `${predictionAmount}%`;
+                    break;
+                
+                case "RE":
+                    ReBar.style.width = `${predictionAmount}%`;
+                    break;
+                
+                case "MI":
+                    MiBar.style.width = `${predictionAmount}%`;
+                    break;
+                
+                case "FA":
+                    FaBar.style.width = `${predictionAmount}%`;
+                    break;
+                
+                case "SOL":
+                    SolBar.style.width = `${predictionAmount}%`;
+                    break;
+                
+                case "LA":
+                    LaBar.style.width = `${predictionAmount}%`;
+                    break;
+                
+                case "SI":
+                    SiBar.style.width = `${predictionAmount}%`;
+                    break;
+                
+                case "Ruido de fondo":
+                    NoiseBar.style.width = `${predictionAmount}%`;
+                    break;
+                
+            }
         }
     }, {
         includeSpectrogram: true, // in case listen should return result.spectrogram
